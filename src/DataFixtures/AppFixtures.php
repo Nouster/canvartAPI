1073296b9736827efba938da7f2806d8e625f07f
Nouster <djebali.mohamed88@gmail.com>
2023-06-25 17:31:16 +0200

    started fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,94 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Address;
+use App\Entity\CategoryNFT;
+use App\Entity\CollectionNFT;
+use App\Entity\NFT;
+use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    const NBCATEGORY = 7;
+    const NBCOLLECTION = 10;
+    const NBADDRESS = 3;
+    const NBNFT = 10;
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create();
+
+        $categories = [];
+        for ($i=0; $i < self::NBCATEGORY; $i++) {
+            
+            $category = new CategoryNFT();
+            $category->setName($faker->word());
+            $manager->persist($category);
+            $categories[] = $category;   
+        }
+
+
+        $collections = [];
+        for ($i = 0; $i < self::NBCOLLECTION; $i++) 
+        {
+            $collection = new CollectionNFT();
+            $collection->setName($faker->word());
+            $manager->persist($collection);
+            $collections[] = $collection;
+}
+
+        $adresses = [];
+        for ($i = 0; $i < self::NBADDRESS; $i++) {
+            $address = new Address();
+            $address->setStreet($faker->sentence())
+                    ->setZipCode($faker->randomNumber(4))
+                    ->setCity($faker->word())
+                    ->setCountry(($faker->word()));
+            $manager->persist($address);
+            $addresses[] = $address;
+
+        }
+
+        $userAdmin = new User();
+        $userAdmin->setEmail('test@gmail.com')
+            ->setRoles(["ROLE_ADMIN"])
+            ->setPassword('test')
+            ->setAddress($faker->randomElement($addresses))
+            ->setFirstname('test')
+            ->setLastname('test')
+            ->setGender("female")
+            ->setBirthdate(new DateTime(1970 - 01 - 01));
+        $manager->persist($userAdmin);
+        $users[] = $userAdmin;
+
+        $userRegular = new User();
+        $userRegular->setEmail('testRegularUser@gmail.com')
+            ->setPassword('test')
+            ->setAddress($faker->randomElement($addresses))
+            ->setFirstname('testRegularUser')
+            ->setLastname('testRegularUser')
+            ->setGender("male")
+            ->setBirthdate(new DateTime(1990 - 20 - 10));
+        $manager->persist($userRegular);
+        $users[] = $userRegular;
+
+        $nfts = [];
+        for ($i=0; $i <self::NBNFT ; $i++) { 
+            $nft = new NFT();
+            $nft->setName($faker->word())
+                ->setImg($faker->url())
+                ->setDescription($faker->paragraph())
+                ->setLaunchDate((new DateTime()))
+                ->setLaunchPriceEth($faker->randomFloat())
+                ->setLaunchPriceEuro($faker->randomFloat())
+                ->setCollectionNFT($faker->randomElement($collections));
+
+                $manager->persist($nft);
+                $nfts[] = $nft;
+        }
 
         $manager->flush();
     }
